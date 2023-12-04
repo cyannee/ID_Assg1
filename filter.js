08efaf45b4e8398f45208266e7b801031771c954
cyannee <s10259855@connect.np.edu.sg>
2023-12-04 23:44:20 +0800

    commit 5

---
+++ filter.js
@@ -0,0 +1,21 @@
+function filterItems(category){
+    var items = document.querySelectorAll('.container-merch');
+    items.forEach(function(item){
+        var itemCategory = item.dataset.category;
+        if (category === 'Show All' || itemCategory === category) {
+            item.style.display = 'block';
+        } else{
+            item.style.display = 'none';
+        }
+    });
+    adjustLayout();
+}
+
+
+function showAll(){
+    var items = document.querySelectorAll('.container-merch');
+    items.forEach(function(item) {
+        item.style.display = 'block';
+    });
+    adjustLayout();
+}
